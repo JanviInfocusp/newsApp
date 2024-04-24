0c0e0913ffe5d6c5bb3105f9d096a7a14c52a875
Jahanvi Thakkar <jahanvi@penelope.infocusp.com>
2024-04-24 17:27:53 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,138 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+import React, { useState } from "react";
+import Navbar from "./Components/Navbar";
+import News from "./Components/News";
+import { Routes, Route } from "react-router-dom";
+import LoadingBar from "react-top-loading-bar";
+
+const App = () => {
+  const [progress, setProgress] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <LoadingBar
+        color="#f11946"
+        height={3}
+        progress={progress}
+        onLoaderFinished={() => setProgress(100)}
+      />
+      <div>
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <News
+                setProgress={setProgress}
+                key="general"
+                pageSize={5}
+                country="in"
+                category="general"
+              />
+            }
+          />
+          <Route
+            exact
+            path="/business"
+            element={
+              <News
+                setProgress={setProgress}
+                key="business"
+                pageSize={5}
+                country="in"
+                category="business"
+              />
+            }
+          />
+          <Route
+            exact
+            path="/entertainment"
+            element={
+              <News
+                setProgress={setProgress}
+                key="entertainment"
+                pageSize={5}
+                country="in"
+                category="entertainment"
+              />
+            }
+          />
+
+          <Route
+            exact
+            path="/general"
+            element={
+              <News
+                setProgress={setProgress}
+                key="general"
+                pageSize={5}
+                country="in"
+                category="general"
+              />
+            }
+          />
+
+          <Route
+            exact
+            path="/health"
+            element={
+              <News
+                setProgress={setProgress}
+                key="health"
+                pageSize={5}
+                country="in"
+                category="health"
+              />
+            }
+          />
+
+          <Route
+            exact
+            path="/science"
+            element={
+              <News
+                setProgress={setProgress}
+                key="science"
+                pageSize={5}
+                country="in"
+                category="science"
+              />
+            }
+          />
+
+          <Route
+            exact
+            path="/sports"
+            element={
+              <News
+                setProgress={setProgress}
+                key="sports"
+                pageSize={5}
+                country="in"
+                category="sports"
+              />
+            }
+          />
+
+          <Route
+            exact
+            path="/technology"
+            element={
+              <News
+                setProgress={setProgress}
+                key="technology"
+                pageSize={5}
+                country="in"
+                category="technology"
+              />
+            }
+          />
+        </Routes>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
